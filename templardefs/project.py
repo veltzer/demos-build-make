c924017392d92de131f801cf54685cb32eb2f5b0
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-23 11:21:20 +0300

    python fixes

--- templardefs/project.py
+++ templardefs/project.py
@@ -3,12 +3,12 @@ project definitions for templar
 '''
 
 def populate(d):
-	d.project_name='demos-make'
-	d.project_long_description='Demos for GNU make'
-	d.project_year_started='2010'
-	d.project_description='TBD'
+    d.project_name='demos-make'
+    d.project_long_description='Demos for GNU make'
+    d.project_year_started='2010'
+    d.project_description='TBD'
 
 def getdeps():
-	return [
-		__file__, # myself
-	]
+    return [
+        __file__, # myself
+    ]
