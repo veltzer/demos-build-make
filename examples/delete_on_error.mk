61975e853d73e71ab6396649a214671b6ba598a6
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-04 05:09:34 +0200

    added a comment

--- examples/delete_on_error.mk
+++ examples/delete_on_error.mk
@@ -4,6 +4,11 @@
 #
 # In this example, if you remove the .DELETE_ON_ERROR line, make will revert
 # to it's default behaviour and WILL NOT remove the erroneous /tmp/tmp file.
+#
+# the recommendation is to use this in any real life project as it ensures
+# that if you use tools that leave cruft that cruft will be cleaned.
+# naturally, if the tools leave cruft in the form of files whose name
+# is not the target file then that cruft will not be removed by make(1)
 
 .DELETE_ON_ERROR:
 
