b23044e3edf95052951e36c65ad5894fd9c51704
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-20 14:38:31 +0300

    small note

--- examples/subtract_lists.mk
+++ examples/subtract_lists.mk
@@ -1,6 +1,8 @@
 # This is an example of how to subtract lists one from the other
 # Notes that the list that is subtracted can have elements
 # which dont exist in the original list
+# Also note the order between the arguments: first argument is the list of elements
+# to be subtracted while the second list is the list from which they are subtracted.
 
 L1:=a b c d e f g
 L2:=b d f x
