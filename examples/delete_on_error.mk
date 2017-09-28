966cdb7c5372c103d4013c66988b163c5b5ae7dc
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-28 19:18:22 +0300

    more notes on deleting on error

--- examples/delete_on_error.mk
+++ examples/delete_on_error.mk
@@ -9,9 +9,23 @@
 # that if you use tools that leave cruft that cruft will be cleaned.
 # naturally, if the tools leave cruft in the form of files whose name
 # is not the target file then that cruft will not be removed by make(1)
+#
+# Note that this only works *WHEN YOUR RULE MAKES THAT TARGET THAT MAKE
+# KNOWS ABOUT*. This is usually the case but still, in a lot of cases
+# in order to make target A you have a more than one line and you
+# create other files like B, C and D. In that case when the rule fails
+# make will remove A for you but B, C and D will remain as cruft.
+# 
+# In order to remove them you need to follow the patten below.
 
 .DELETE_ON_ERROR:
 
 /tmp/tmp:
 	$(info doing [$@])
 	cat /etc/passwd /tmp/notexists > $@
+
+/tmp/tmp2:
+	$(info doing [$@])
+	cat /etc/passwd > /tmp/A || { rm /tmp/A && false; }
+	cat /etc/group > /tmp/B || { rm /tmp/A /tmp/B && false; }
+	cat_mistake /tmp/A /tmp/B > $@ || { rm /tmp/A /tmp/B && false; }
