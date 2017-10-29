7eb2b343981f49bd3ff65fa396a1a717cc5b2db1
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-29 12:45:20 +0200

    better example

--- examples/silent_nothing_to_be_done_for.mk
+++ examples/silent_nothing_to_be_done_for.mk
@@ -1,4 +1,7 @@
-# This makefile shows how to shut make up in various ways...
+# In a lot of scenarios when make has nothing to do you get a message like
+# make: Nothing to be done for 'all'.
+# Sometimes you hate thing message and don't want it to appear.
+# This example shows various solutions to this problem.
 #
 # References:
 # - http://stackoverflow.com/questions/3417391/suppress-nothing-to-be-done-for-all
@@ -8,34 +11,51 @@
 .PHONY: problem
 problem:
 
-# this is the most efficient solution since : is a shell built-in and a no-op.
+# the best solution, but since ':' is rarely understood by people
+# I would go with the next one
+# no message from make
+# ':' is a load overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution1
 solution1:
 	@:
 
-# now there is no message from make and the 'true' command is not shown
-# true is a shell builtin as well.
+# the best solution which is well understood by everyone.
+# no message from make
+# 'true' is low overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution2
 solution2:
 	@true
 
-# same, the exec seems to be superfluous
+# the 'exec' is superfluous in this solution.
+# no message from make
+# 'exec' is a low overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution3
 solution3:
 	@exec true
 
-# same
+# a little too compilcated for my taste
+# no message from make
+# 'exit' is a low overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution4
 solution4:
 	@exit 0
 
-# the most inefficient solution number 1
+# inefficient solution (-n causes echo not to really echo anything)
+# no message from make
+# 'echo' is a low overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution5
 solution5:
 	@echo -n
 
-# the most inefficient solution number 2
+# inefficient solution (echoing newline into /dev/null)
+# no message from make
+# 'echo' is a low overhead, shell-builtin, returns ok status code
+# the '@' is to not show the command being executed
 .PHONY: solution6
 solution6:
 	@echo > /dev/null
-
