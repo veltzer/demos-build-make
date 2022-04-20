552135294651e7dcf4a248c02e8766ec12ab7d0b
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-20 17:57:09 +0300

    better helpers and years

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Demos for GNU make
 
 TBD
 
-	Mark Veltzer <mark.veltzer@gmail.com>, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
+	Mark Veltzer <mark.veltzer@gmail.com>, 2010 - 2022
--- config/project.py
+++ config/project.py
@@ -4,5 +4,7 @@ project_name="demos-make"
 project_long_description="Demos for GNU make"
 project_year_started="2010"
 project_description="TBD"
-project_copyright_years = ", ".join(
-    map(str, range(int(project_year_started), datetime.datetime.now().year + 1)))
+project_year_now=str(datetime.datetime.now().year)
+project_copyright_years=", ".join(
+    map(str, range(int(project_year_started), int(project_year_now)+1)))
+project_copyright_years_short = f"{project_year_started} - {project_year_now}"
