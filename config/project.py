7deb5365f18517810b18a8278f9e6eb8f4923814
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-20 18:04:00 +0300

    fixes years

--- config/project.py
+++ config/project.py
@@ -1,9 +1,9 @@
-import datetime
-
 project_name="demos-make"
 project_long_description="Demos for GNU make"
 project_year_started="2010"
 project_description="TBD"
+
+import datetime
 project_year_now=str(datetime.datetime.now().year)
 project_copyright_years=", ".join(
     map(str, range(int(project_year_started), int(project_year_now)+1)))
