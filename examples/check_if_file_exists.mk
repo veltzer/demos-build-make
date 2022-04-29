a80e5ad3465e099d07437dee0136d081c6b9131f
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-29 06:47:09 +0300

    added ref

--- examples/check_if_file_exists.mk
+++ examples/check_if_file_exists.mk
@@ -1,4 +1,7 @@
 # This is a basic example to be used as basis for building other examples
+# 
+# References:
+# - https://stackoverflow.com/questions/5553352/how-do-i-check-if-file-exists-in-makefile-so-i-can-delete-it
 
 check-have-file = $(shell if test -f $1; then echo 1; else echo 0; fi)
 
