79181ae316a60b8e27da8eaba43d427b500cd228
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-30 20:29:31 +0300

    .mypy.ini sync

--- examples/depend_on_the_makefile_solution_3.mk
+++ examples/depend_on_the_makefile_solution_3.mk
@@ -3,7 +3,6 @@
 # References:
 # - https://www.gnu.org/software/make/manual/html_node/Special-Variables.html
 
-.EXTRA_PREREQS+=$(foreach mk, ${MAKEFILE_LIST},$(abspath ${mk}))
 
 ALL:=/tmp/copy_of_passwd /tmp/copy_of_hosts
 
@@ -19,3 +18,11 @@ clean:
 
 /tmp/copy_of_hosts: /etc/hosts
 	cp $< $@
+
+# do you want dependency on the Makefile itself ?
+DO_ALLDEP:=1
+# .EXTRA_PREREQS+=$(foreach mk, ${MAKEFILE_LIST},$(abspath ${mk}))
+# .EXTRA_PREREQS=depend_on_the_makefile_solution_3.mk
+ifeq ($(DO_ALLDEP),1)
+.EXTRA_PREREQS+=$(foreach mk, ${MAKEFILE_LIST},$(abspath ${mk}))
+endif # DO_ALLDEP
