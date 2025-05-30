131a3dddcc82ef30cb39d0c958335de5fb5cfb8b
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-30 20:32:14 +0300

    no commit message given

--- examples/depend_on_the_makefile_solution_3.mk
+++ examples/depend_on_the_makefile_solution_3.mk
@@ -1,5 +1,8 @@
 # This is the best solution for adding a dependency to all receipes
 #
+# It doesn't matter where you put .EXTRA_PREREQS, it will work everywhere.
+# It will apply to all types of rules in makefiles (regular, pattern, etc...)
+#
 # References:
 # - https://www.gnu.org/software/make/manual/html_node/Special-Variables.html
 
@@ -19,10 +22,11 @@ clean:
 /tmp/copy_of_hosts: /etc/hosts
 	cp $< $@
 
-# do you want dependency on the Makefile itself ?
-DO_ALLDEP:=1
+# Here are three ways of using .EXTRA_PREREQS
 # .EXTRA_PREREQS+=$(foreach mk, ${MAKEFILE_LIST},$(abspath ${mk}))
 # .EXTRA_PREREQS=depend_on_the_makefile_solution_3.mk
+# do you want dependency on the Makefile itself ?
+DO_ALLDEP:=1
 ifeq ($(DO_ALLDEP),1)
 .EXTRA_PREREQS+=$(foreach mk, ${MAKEFILE_LIST},$(abspath ${mk}))
 endif # DO_ALLDEP
